427e498b512b2fede4a622403577c07bf7dded3a
enestekin <zenestekin@gmail.com>
2022-11-21 00:26:29 +0300

    cartcontext reducer

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -4,6 +4,8 @@ import {
   onAuthStateChangedListener,
 } from '../utils/firebase';
 
+import { createAction } from '../utils/reducer/reducer.utils';
+
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
@@ -38,7 +40,7 @@ export const UserProvider = ({ children }) => {
   const { currentUser } = state;
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
